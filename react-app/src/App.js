86f22bb240f9b6aa55534c17095ab8155bd60c08
Ieshaham <131932350+Ieshaham@users.noreply.github.com>
2023-07-15 01:02:21 +0000

    starting

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,38 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 
+
+
 const backendHostUrl= 'https://ieshaham-stunning-space-system-ggrvp5x5ggwcx7p-5001.preview.app.github.dev'
 function App() {
+
+  const[name,setName]= useState(0);
+  const[temp,setTemp]= useState(0);
   useEffect(() => {
     (async () => {
       const res = await fetch(
-        `${backendHostUrl}/geeks-firebase-72e6d/us-central1/helloWorld`
+        `${backendHostUrl}/geeks-firebase-72e6d/us-central1/getDayWeather`
+        
       );
 
       const data = await res.json();
 
       console.log("The res: ", data);
+      setTemp(data.data.current.feelslike_c);
+      setName(data.data.location.name);
     })();
   }, []);
 
+
+
+
   return (
+    <div className="outer">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{name}</h3>
+      <h1>{temp}</h1>
+    </div>
     </div>
   );
 }
