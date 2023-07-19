da973216ec9a14330eaaead6073a89aa75e804de
Ieshaham <131932350+Ieshaham@users.noreply.github.com>
2023-07-19 15:59:04 +0000

    add

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,6 +14,12 @@ function App() {
   const[maxtemp_f,setTemp3]= useState(0);
   const[text,setText]=useState(0);
   const[icon,setIcon]=useState(0);
+  const [epotch, setEpotch] = useState("");
+  const [epotch1h, setEpotch1H] = useState("");
+  const [epotch2h, setEpotch2H] = useState("");
+  const [hour0, setHour0] = useState("");
+  const [currenttime, setCurrentTime] = useState("");
+  const [hours, setHours] = useState([]);
   
   useEffect(() => {
     (async () => {
@@ -31,8 +37,18 @@ function App() {
       setIcon("http:"+data.data.current.condition.icon);
       setTemp2(data.data.forecast.forecastday[0].day.mintemp_f);
       setTemp3(data.data.forecast.forecastday[0].day.maxtemp_f);
+      setEpotch(data.data.current.condition.last_updated_epoch);
+      setHour0(data.data.forecast.forecastday[0].hour[0]);
+      setHours(data.data.forecast.forecastday[0].hour);
       
+      // for (let i = 0; i < 24; i++) {
+        //   if (epotch < setEpotch1H(data.data.forecast.forecastday[0].hour[i].time_epoch)) {
+        //     setEpotch1H(data.data.forecast.forecastday[0].hour[i].temp_f);
+        //     setEpotch2H(data.data.forecast.forecastday[0].hour[i + 1].temp_f);
+        //   }
+        // }
     })();
+
   }, []);
 
 
@@ -47,7 +63,25 @@ function App() {
       <div className="temp">{temp}</div>
       <div className="conditionText"><p>{text}</p></div>
       <div className="min-maxtemp"> <p>L:{mintemp_f} &nbsp;&nbsp;&nbsp;&nbsp; H: {maxtemp_f}</p> </div>
-   
+    <div className="currenttemp">
+            <b>Current Hour</b>
+            {temp} °F
+          </div>
+          <div className="nexthour">
+            <b>Next Hour</b>
+            {epotch1h} °F
+          </div>
+          <div className="secondhour">
+            <b>Next 2nd Hour</b>
+            {epotch2h} °F
+          </div>
+          {hours.map((hour) => (
+            <div className="list-group">
+              {!!hour.now && "Now"}
+              {hour.formatted_time}
+              {hour.temp_f}
+            </div>
+          ))}
     </div>
     </div>
   );
